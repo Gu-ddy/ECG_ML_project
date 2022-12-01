fca4aeb3431f71fb1fbfe34ec2249533407226c3
lbarberi1927 <lbarberi@student.ethz.ch>
2022-12-01 16:52:41 +0100

    stack regressor

--- Models/autoencoder_NN.py
+++ Models/autoencoder_NN.py
@@ -32,6 +32,7 @@ class AutoEncoderClassification(
     def __init__(self):
         super(AutoEncoderClassification, self).__init__()
         self.encoder = tf.keras.Sequential([
+            layers.Input(shape=(86,)),
             layers.Dense(128, activation="relu"),
             layers.Dense(64, activation="relu"),
             layers.Dense(32, activation="relu"),
@@ -41,7 +42,9 @@ class AutoEncoderClassification(
         self.decoder = tf.keras.Sequential([
             layers.Dense(8, activation="relu"),
             layers.Dense(16, activation="relu"),
-            layers.Dense(26, activation="sigmoid")])
+            layers.Dense(32, activation="relu"),
+            layers.Dense(64, activation="relu"),
+            layers.Dense(86, activation="relu")])
 
     def call(self, x):
         encoded = self.encoder(x)
@@ -69,19 +72,19 @@ def separate_dataset(X_train, y_train, class_):
 
 if __name__ == "__main__":
     leo_training_data_path = (
-        "/Users/leonardobarberi/Desktop/ETH/Semester_1/AML/task2/features_data_merged_intervals.csv"
+        "/Users/leonardobarberi/Desktop/ETH/Semester_1/AML/task2/X_train_Davide.csv"
     )
     leo_y_train_path = (
         "/Users/leonardobarberi/Desktop/ETH/Semester_1/AML/task2/y_train.csv"
     )
     leo_X_test_path = (
-        "/Users/leonardobarberi/Desktop/ETH/Semester_1/AML/task2/X_test_data_merged_intervals.csv"
+        "/Users/leonardobarberi/Desktop/ETH/Semester_1/AML/task2/X_test_Davide.csv"
     )
 
     X_train = pd.read_csv(
         leo_training_data_path
     )
-    X_train.drop("Unnamed: 0", axis=1, inplace=True)
+
     y = pd.read_csv(
         leo_y_train_path
     )
@@ -89,11 +92,6 @@ if __name__ == "__main__":
     X_test = pd.read_csv(
         leo_X_test_path
     )
-    X_test.drop(["Unnamed: 0", "Unnamed: 0.1"], axis=1, inplace=True)
-    imputer = SimpleImputer(strategy='median')
-    imputer.fit(pd.concat((X_train, X_test), axis=0))
-    X_train = imputer.transform(X_train)
-    X_test = imputer.transform(X_test)
 
     scaler = StandardScaler()
     scaler.fit(np.concatenate((X_train, X_test), axis=0))
@@ -103,13 +101,6 @@ if __name__ == "__main__":
     X_train = pd.DataFrame(X_train)
     X_test = pd.DataFrame(X_test)
 
-    # don't know if this is necessary
-    # X_train = tf.cast(X_train, tf.float32)
-    # X_test = tf.cast(X_test, tf.float32)
-
-    autoencoder = AutoEncoderClassification()
-    autoencoder.compile(optimizer='Adam', loss='mae')
-
     normal_data, anomalous_data = separate_dataset(X_train, y, class_=0)
     x_normal = normal_data.iloc[:, 0:-1]
     y_normal = normal_data.iloc[:, -1]
@@ -117,20 +108,28 @@ if __name__ == "__main__":
     x_train, x_val, y_train, y_val = train_test_split(x_normal,
                                                       y_normal,
                                                       train_size=0.8,
-                                                      random_state=21,
-                                                      stratify=y_normal)
+                                                      random_state=21)
 
-    history = autoencoder.fit(x_normal, x_normal,
+    # don't know if this is necessary
+    x_train = tf.cast(x_normal, tf.float32)
+    x_val = tf.cast(x_val, tf.float32)
+    # X_test = tf.cast(X_test, tf.float32)
+
+    autoencoder = AutoEncoderClassification()
+    autoencoder.compile(optimizer='Adam', loss='mae')
+
+
+    history = autoencoder.fit(x_train, x_train,
                               epochs=200,  # change
                               batch_size=512,
                               validation_data=(x_val, x_val),
                               shuffle=True)
 
-    reconstructions = autoencoder.predict(x_normal)
-    train_loss = tf.keras.losses.mae(reconstructions, x_normal)
+    reconstructions = autoencoder.predict(x_train)
+    train_loss = tf.keras.losses.mae(reconstructions, x_train)
 
     threshold = np.mean(train_loss) + np.std(train_loss)
     print("Threshold: ", threshold)
 
-    preds = predict(autoencoder, x_val, threshold)
+    preds = predict(autoencoder, x_normal, threshold)
     print_stats(preds, y_val)
